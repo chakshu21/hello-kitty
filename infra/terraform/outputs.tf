385d1edf51c657e935ab1511c70ef2d96b349686
stackgen-automation <automation@stackgen.com>
2024-09-28 09:55:13 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,16 +1,4 @@
 
-output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_chakshu_salgotra_10_arn" {
-  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.arn
-  sensitive = false
-}
-
-
-output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_chakshu_salgotra_10_name" {
-  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.name
-  sensitive = false
-}
-
-
 output "aws_s3_chakshu_salgotra_10_arn" {
   value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.arn
   sensitive = false
@@ -67,5 +55,17 @@ output "aws_iam_role_hello_kitty_role_chakshu_salgotra_10_arn" {
 
 output "aws_iam_role_hello_kitty_role_chakshu_salgotra_10_name" {
   value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.name
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_chakshu_salgotra_10_name" {
+  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.name
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_chakshu_salgotra_10_arn" {
+  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.arn
   sensitive = false
 }
